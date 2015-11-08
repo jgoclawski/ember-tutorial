32246f809956f9b97315226bbbb25ec062837677
Jakub Gocławski <gocek13@gmail.com>
2015-11-09 00:32:07 +0100

    Add Todos with Localforage adapter.

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,23 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        //return this.store.find('todo');
-        return [
-            {
-              id: 1,
-              title: 'Learn Ember.js',
-              isCompleted: true
-            },
-            {
-              id: 2,
-              title: '...',
-              isCompleted: false
-            },
-            {
-              id: 3,
-              title: 'Profit!',
-              isCompleted: false
-            }
-        ];
+        return this.store.findAll('todo');
+        //return [
+        //    {
+        //      id: 1,
+        //      title: 'Learn Ember.js',
+        //      isCompleted: true
+        //    },
+        //    {
+        //      id: 2,
+        //      title: '...',
+        //      isCompleted: false
+        //    },
+        //    {
+        //      id: 3,
+        //      title: 'Profit!',
+        //      isCompleted: false
+        //    }
+        //];
     }
 });
