70a4dbe8abfb617f710d20d411ae7ec0b18e04b0
Jakub Gocławski <gocek13@gmail.com>
2015-11-14 23:15:00 +0100

    Enable editing of a todo.

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isEditing: false,
+
   actions: {
     complete(value) {
       var model = this.get('todo');
       model.set('isCompleted', value);
       model.save();
+    },
+    edit() {
+      this.set('isEditing', true);
     }
   }
 });
