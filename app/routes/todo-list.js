cb45e2c23254c9281ccdd71e6b2dc703e913046d
Jakub Gocławski <gocek13@gmail.com>
2015-11-22 23:25:43 +0100

    Clean up code.

--- app/routes/todo-list.js
+++ app/routes/todo-list.js
@@ -3,22 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('todo');
-        //return [
-        //    {
-        //      id: 1,
-        //      title: 'Learn Ember.js',
-        //      isCompleted: true
-        //    },
-        //    {
-        //      id: 2,
-        //      title: '...',
-        //      isCompleted: false
-        //    },
-        //    {
-        //      id: 3,
-        //      title: 'Profit!',
-        //      isCompleted: false
-        //    }
-        //];
     }
 });
