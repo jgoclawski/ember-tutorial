13f97d47e2a6e549ea8c845c098d9058039e4e05
Jakub Gocławski <gocek13@gmail.com>
2016-01-06 19:36:48 +0100

    Add active todos route.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('todo-list', { path: '/' }, function () {
-    // additional child routes will go here later
+    this.route('active');
   });
 });
 
---
+++ tests/unit/routes/todo-list/active-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:todo-list/active', 'Unit | Route | todo list/active', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
